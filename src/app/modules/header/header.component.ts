46f52903870da9b57ee7bf0c19a85f26e29b8f09
witch <witchrainforest@gmail.com>
2023-01-09 21:57:16 +0000

    update

--- src/app/modules/header/header.component.ts
+++ src/app/modules/header/header.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { faPhotoFilm } from '@fortawesome/free-solid-svg-icons';
+import { faSun } from '@fortawesome/free-solid-svg-icons';
+import { faWindowRestore } from '@fortawesome/free-solid-svg-icons';
+import { faListOl } from '@fortawesome/free-solid-svg-icons';
 
 
 @Component({
@@ -9,6 +13,12 @@ import { Component, OnInit } from '@angular/core';
 export class HeaderComponent implements OnInit {
 
   title:String = "Rainforest Information System";
+
+    filmIcon = faPhotoFilm;
+  sunIcon = faSun;
+  windowIcon = faWindowRestore
+  listIcon = faListOl
+
 
   constructor( ) {
   } 

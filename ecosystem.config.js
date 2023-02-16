1d83eedff5af429e8bbc679e5412573aed63ed09
witch <witchrainforest@gmail.com>
2023-02-16 16:54:12 +0000

    check

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,10 +1,10 @@
 module.exports = {
   apps: [
     {
-      name: 'angular-openlayers',
+      name: 'client',
       script: 'serve',
       env: {
-        PM2_SERVE_PATH: 'dist/angular-openlayers',
+        PM2_SERVE_PATH: 'dist/client',
         PM2_SERVE_PORT: +(process.env.PORT || 80)
       }
     }

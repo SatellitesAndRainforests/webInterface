43126f139a970bc6207de58668292c35586fae67
witch <witchrainforest@gmail.com>
2023-02-18 22:08:39 +0000

    livestream dialog

--- src/app/components/network/network.component.ts
+++ src/app/components/network/network.component.ts
@@ -5,6 +5,9 @@ import { first } from "rxjs/operators";
 import { Subscription } from 'rxjs';
 import {NgbModal, ModalDismissReasons} from '@ng-bootstrap/ng-bootstrap';
 
+import {MatDialog, MatDialogRef} from '@angular/material/dialog';
+
+
 
 
 @Component({
@@ -14,21 +17,33 @@ import {NgbModal, ModalDismissReasons} from '@ng-bootstrap/ng-bootstrap';
 })
 export class NetworkComponent implements OnInit {
 
+  //nightVisionCameraIsOnline:boolean = false;
   nightVisionCameraIsOnline:boolean = false;
   cameraTrapIsOnline:boolean = false;
   mobileIsOnline:boolean = false;
   liveStreamIsOn:boolean = false;
   nvCamIsCapturing = false;
 
-
   closeResult = '';
 
-
-  constructor( private captureService: CaptureService, private modalService: NgbModal) { }
+  constructor( private captureService: CaptureService, private modalService: NgbModal, public dialog: MatDialog) { }
 
   ngOnInit(): void {
     this.isNightVisionCameraOnline();
   }
+
+  openDialog(): void {
+    const dialogRef = this.dialog.open(DialogComponent, {
+      height: '85%',
+      width: '100%',
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log('The dialog was closed');
+      // reset nvCamService.service
+    });
+  }
+
 
 
   isNightVisionCameraOnline() {
@@ -58,9 +73,9 @@ export class NetworkComponent implements OnInit {
 
   retrieveImages() {
     this.captureService.retrieveNvCamImages().subscribe( (response: Boolean) => {
-      
+
       console.log(response.valueOf()); //response is positive if command was successful.
-    
+
     });
 
   }
@@ -94,15 +109,25 @@ export class NetworkComponent implements OnInit {
 
 
 
+}
 
 
+@Component({
+  selector: 'livestream-dialog',
+  templateUrl: 'dialog.component.html',
+  styleUrls: ['dialog.component.css']
+})
+export class DialogComponent {
+  constructor(public dialogRef: MatDialogRef<DialogComponent>) {
+  }
 
-
-
-
-
+  closeDialog(): void {
+    this.dialogRef.close();
+  }
 
 }
+
+
 
 
 
